afed85ce47558f403d2ffa5f43a5de1ca6320a97
Andrew de Quincey <adq@lidskialf.net>
2014-03-26 23:46:30 +0000

    Set userid on trackAction

--- TVSquaredCollectorSample/TVSViewController.m
+++ TVSquaredCollectorSample/TVSViewController.m
@@ -39,6 +39,7 @@
 }
 
 - (IBAction)trackAction:(id)sender {
+    _collector.userId = _txt_userid.text;
     [_collector track:_txt_actionname.text product:_txt_product.text orderid:_txt_orderid.text revenue:[_txt_revenue.text floatValue] promocode:_txt_promocode.text];
 }
 @end
